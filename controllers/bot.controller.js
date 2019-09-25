f8376ae3c12d47296a94953a857eb5840b93c742
Fran Enriquez <fjenriglez@gmail.com>
2019-09-25 20:14:31 +0200

    fix deploy heroku

--- controllers/bot.controller.js
+++ controllers/bot.controller.js
@@ -1,7 +1,12 @@
 const createError = require('http-errors')
 
-module.exports.command = (req, res, next) => {
-  const prueba = {"me ha llegado": req.body}
+const API_SLACK_BOT = process.env.API_SLACK_BOT
 
+module.exports.command = (req, res, next) => {
+  const prueba = {
+    "me ha llegado": req.body,
+    "API": API_SLACK_BOT
+  }
+  console.log(JSON.stringify(prueba))
   res.status(201).json(prueba)
 }
